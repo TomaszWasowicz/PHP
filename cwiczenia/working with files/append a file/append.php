86a172735bcf6e9ea22c2b85884fd66bec4f23f8
benz_ <tomasz.wasowicz@gmail.com>
2021-07-18 21:05:06 +0200

    18.07.21

--- cwiczenia/working with files/append a file/append.php
+++ cwiczenia/working with files/append a file/append.php
@@ -1,6 +1,6 @@
 <?php
-$mojPlik = "text.txt";
-$zmienna = fopen($mojPlik, 'a'); // 'a' dopisuje tekst nie nadpisuje jak 'w'
+$file = "text.txt";
+$zmienna = fopen($file, 'a'); // 'a' dopisuje tekst nie nadpisuje jak 'w'
 fwrite($zmienna, "Prosty tekst ktory dopisuje\n");
 fclose($zmienna);
 
